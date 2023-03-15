16d56559a21a600425e3d8c6263919dcde651b20
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-03-15 17:19:30 +0000

    Install nodemon and add bootstrap CDN links.

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
+app.route('/')
+  .get((req, res) => {
+    res.render('home');
+  })
+
 
 app.listen(3000, function() {
     console.log('App is running on port 3000.');
